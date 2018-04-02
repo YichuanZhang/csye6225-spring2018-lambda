49fc0e1f6a357ae553a8d160769369d9a7a553aa
yichuan <yczneu@gmail.com>
2018-04-02 17:14:20 -0400

    update lambda

--- src/main/java/LogEvent.java
+++ src/main/java/LogEvent.java
@@ -1,12 +1,45 @@
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
-
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import com.amazonaws.services.simpleemail.model.*;
+import java.util.UUID;
 
 public class LogEvent implements RequestHandler<SNSEvent, Object> {
+
+
+    private DynamoDB dynamoDb;
+    private String DYNAMODB_TABLE_NAME = "csye6225";
+    private Regions REGION = Regions.US_EAST_1;
+    static final String FROM = "noreply@csye6225-spring2018-zhangyichu.me";
+    static final String SUBJECT = "Reset Password Link";
+    static  String HTMLBODY = "<h1>Csye 6225 Password reset link</h1>"
+            + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+            + "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>"
+            + "AWS SDK for Java</a>";
+
+    private String textBody;
+
+
+    private void initDynamoDbClient() {
+//        String accessKey = System.getenv("accessKey");
+//        String secretKey = System.getenv("secretKey");
+
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        client.setRegion(Region.getRegion(REGION));
+        this.dynamoDb = new DynamoDB(client);
+    }
+
 
     public Object handleRequest(SNSEvent request, Context context) {
 
@@ -23,6 +56,73 @@ public class LogEvent implements RequestHandler<SNSEvent, Object> {
         timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss").format(Calendar.getInstance().getTime());
 
         context.getLogger().log("Invocation completed: " + timeStamp);
+
+        String username = request.getRecords().get(0).getSNS().getMessage();
+        String token = UUID.randomUUID().toString();
+        this.initDynamoDbClient();
+        Item existUser = this.dynamoDb.getTable(DYNAMODB_TABLE_NAME).getItem("Id", username);
+
+        if (existUser == null) {
+            this.dynamoDb.getTable(DYNAMODB_TABLE_NAME)
+                    .putItem(
+                            new PutItemSpec().withItem(new Item()
+                                    .withString("Id", username)
+                                    .withString("token", token)
+                                    .withLong("TTL", 1200)));
+            textBody = "https://csye6225-spring2018.com/reset?email=" + username + "&token=" + token;
+            try {
+                AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
+                        .withRegion(Regions.US_EAST_1).build();
+
+                String body = HTMLBODY + "<p> "+textBody+"</p>";
+                SendEmailRequest emailRequest = new SendEmailRequest()
+                        .withDestination(
+                                new Destination().withToAddresses(username))
+                        .withMessage(new Message()
+                                .withBody(new Body()
+                                        .withHtml(new Content()
+                                                .withCharset("UTF-8").withData(body))
+                                        .withText(new Content()
+                                                .withCharset("UTF-8").withData(textBody)))
+                                .withSubject(new Content()
+                                        .withCharset("UTF-8").withData(SUBJECT)))
+                        .withSource(FROM);
+                client.sendEmail(emailRequest);
+
+
+                System.out.println("Email sent successfully!");
+            } catch (Exception ex) {
+                System.out.println("The email was not sent. Error message: "
+                        + ex.getMessage());
+            }
+        } else {
+
+            try {
+                AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
+                        .withRegion(Regions.US_EAST_1).build();
+
+                String body = HTMLBODY + "<p>Password reset link already sent</p>";
+
+                SendEmailRequest emailRequest = new SendEmailRequest()
+                        .withDestination(
+                                new Destination().withToAddresses(username))
+                        .withMessage(new Message()
+                                .withBody(new Body()
+                                        .withHtml(new Content()
+                                                .withCharset("UTF-8").withData(body))
+                                )
+                                .withSubject(new Content()
+                                        .withCharset("UTF-8").withData(SUBJECT)))
+                        .withSource(FROM);
+                client.sendEmail(emailRequest);
+
+
+                System.out.println("Email sent successfully!");
+            } catch (Exception ex) {
+                System.out.println("The email was not sent. Error message: "
+                        + ex.getMessage());
+            }
+        }
 
         return null;
     }
